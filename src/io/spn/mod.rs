03a3ac1bdefde397f0bf6ef1719f3b98e9a4a1e7
mrbuche <dev@mrbuche.com>
2024-06-25 18:15:11 -0600

    might be easier to use?

--- src/io/spn/mod.rs
+++ src/io/spn/mod.rs
@@ -12,10 +12,7 @@ pub fn register_module(_py: Python<'_>, parent_module: &PyModule) -> PyResult<()
 
 #[pyclass]
 pub struct Spn {
-    data: Vec<u8>,
-    nelx: usize,
-    nely: usize,
-    nelz: usize,
+    data: Vec<Vec<Vec<u8>>>,
     scale: f64,
 }
 
@@ -26,16 +23,16 @@ impl Spn {
     }
     #[new]
     pub fn init(file_path: &str, nelx: usize, nely: usize, nelz: usize, scale: f64) -> Self {
-        let data = BufReader::new(File::open(file_path).expect("File was not found."))
+        let flat = BufReader::new(File::open(file_path).expect("File was not found."))
             .lines()
             .map(|line| line.unwrap().parse().unwrap())
-            .collect();
-        Self {
-            data,
-            nelx,
-            nely,
-            nelz,
-            scale,
-        }
+            .collect::<Vec<u8>>();
+        let mut data = vec![vec![vec![0; nelx]; nely]; nelz];
+        data.iter_mut()
+            .flatten()
+            .flatten()
+            .zip(flat.iter())
+            .for_each(|(data_entry, flat_entry)| *data_entry = *flat_entry);
+        Self { data, scale }
     }
 }
