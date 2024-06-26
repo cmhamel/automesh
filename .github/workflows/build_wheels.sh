6e72599c983fa514b65f5979ec69e5be7b879b87
mrbuche <dev@mrbuche.com>
2024-06-26 13:02:59 -0600

    fixed???

--- .github/workflows/build_wheels.sh
+++ .github/workflows/build_wheels.sh
@@ -3,7 +3,7 @@ set -e -x
 
 for PYBIN in /opt/python/cp3[891]*/bin; do
     "${PYBIN}/pip" install cffi maturin
-    "${PYBIN}/maturin" build --features python -i "${PYBIN}/python" --release
+    "${PYBIN}/maturin" build -i "${PYBIN}/python" --release
 done
 
 for wheel in target/wheels/*.whl; do
