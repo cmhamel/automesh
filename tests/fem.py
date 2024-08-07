ccfb0fb0212317b797f6137580b15a25a1938337
mrbuche <dev@mrbuche.com>
2024-08-07 14:33:46 -0600

    todo

---
+++ tests/fem.py
@@ -0,0 +1,13 @@
+from automesh import Spn
+
+
+nel = [3, 5, 4]
+scale = [1.2, 2.3, 0.4]
+translate = [-0.3, 1.1, 0.5]
+
+
+def test_write_inp():
+    spn = Spn('tests/input/f.spn', nel)
+    fem = spn.as_finite_elements(scale, translate)
+    fem.write_inp("target/f.inp")
+    assert False
