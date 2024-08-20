72c281548675f6498666d118e76faffa1e7fdae1
Chad Hovey <hovey@me.com>
2024-08-20 14:19:53 -0400

    refactor for TDD approach

--- tests/voxels.py
+++ tests/voxels.py
@@ -116,7 +116,7 @@ class Double(Example):
 class DoubleY(Example):
     """A specific example of a double voxel, coursed along the y-axis."""
 
-    figure_title: str = "Double Y Element Global Node Numbers and Coordinates"
+    figure_title: str = COMMON_TITLE + "DoubleY"
     file_stem: str = "double_y"
     segmentation = np.array(
         [
@@ -147,7 +147,7 @@ class DoubleY(Example):
 class Triple(Example):
     """A specific example of a triple voxel."""
 
-    figure_title: str = "Triple Element Global Node Numbers and Coordinates"
+    figure_title: str = COMMON_TITLE + "Triple"
     file_stem: str = "triple"
     segmentation = np.array(
         [
@@ -185,7 +185,7 @@ class Triple(Example):
 class Quadruple(Example):
     """A specific example of a quadruple voxel."""
 
-    figure_title: str = "Quadruple Element Global Node Numbers and Coordinates"
+    figure_title: str = COMMON_TITLE + "Quadruple"
     file_stem: str = "quadruple"
     segmentation = np.array(
         [
@@ -228,9 +228,7 @@ class QuadrupleVoid(Example):
     segmentation being void.
     """
 
-    figure_title: str = (
-        "Quadruple with Voids Element Global Node Numbers and Coordinates"
-    )
+    figure_title: str = COMMON_TITLE + "QuadrupleVoid"
     file_stem: str = "quadruple_void"
     segmentation = np.array(
         [
@@ -269,7 +267,7 @@ class QuadrupleVoid(Example):
 class Cube(Example):
     """A specific example of a (2 x 2 x 2) voxel cube."""
 
-    figure_title: str = "Cube Element Global Node Numbers and Coordinates"
+    figure_title: str = COMMON_TITLE + "Cube"
     file_stem: str = "cube"
     segmentation = np.array(
         [
@@ -330,8 +328,8 @@ class Cube(Example):
 class LetterF(Example):
     """A minimal letter F example."""
 
-    figure_title: str = "Letter F minimal"
-    file_stem: str = "letter_f_minimal"
+    figure_title: str = COMMON_TITLE + "LetterF"
+    file_stem: str = "letter_f"
     segmentation = np.array(
         [
             [
@@ -552,171 +550,178 @@ def main():
 
     # Create an instance of a specific example
     # user input begin
-    # ex = Single()
-    ex = Double()
-    # ex = DoubleY()
-    # ex = Triple()
-    # ex = Quadruple()
-    # ex = QuadrupleVoid()
-    # ex = Cube()
-    # ex = LetterF()
-    # user input end
+    examples = [
+        Single(),
+        Double(),
+        DoubleY(),
+        Triple(),
+        Quadruple(),
+        QuadrupleVoid(),
+        Cube(),
+        LetterF(),
+    ]
+    for ex in examples:
 
-    # computation
-    output_dir: Final[str] = "~/scratch"
-    output_npy: Final[Path] = (
-        Path(output_dir).expanduser().joinpath(ex.file_stem + ".npy")
-    )
+        # computation
+        output_dir: Final[str] = "~/scratch"
+        output_npy: Final[Path] = (
+            Path(output_dir).expanduser().joinpath(ex.file_stem + ".npy")
+        )
 
-    # visualization
-    visualize: bool = True  # True performs post-processing visualization
-    dpi: Final[int] = 300  # resolution, dots per inch
-    output_png: Final[Path] = (
-        Path(output_dir).expanduser().joinpath(ex.file_stem + ".png")
-    )
-    # el, az, roll = 25, -115, 0
-    el, az, roll = 28, -115, 0
-    # el, az, roll = 60, -121, 0
-    # el, az, roll = 42, -120, 0
+        # visualization
+        visualize: bool = True  # True performs post-processing visualization
+        dpi: Final[int] = 300  # resolution, dots per inch
+        output_png_short = ex.file_stem + ".png"
+        output_png: Final[Path] = (
+            Path(output_dir).expanduser().joinpath(output_png_short)
+        )
+        # el, az, roll = 25, -115, 0
+        # el, az, roll = 28, -115, 0
+        el, az, roll = 63, -110, 0
+        # el, az, roll = 60, -121, 0
+        # el, az, roll = 42, -120, 0
 
-    # io: if the output directory does not already exist, create it
-    output_path = Path(output_dir).expanduser()
-    if not output_path.exists():
-        print(f"Could not find existing output directory: {output_path}")
-        Path.mkdir(output_path)
-        print(f"Created: {output_path}")
-        assert output_path.exists()
+        # io: if the output directory does not already exist, create it
+        output_path = Path(output_dir).expanduser()
+        if not output_path.exists():
+            print(f"Could not find existing output directory: {output_path}")
+            Path.mkdir(output_path)
+            print(f"Created: {output_path}")
+            assert output_path.exists()
 
-    nelz, nely, nelx = ex.segmentation.shape
-    lc = lattice_connectivity(ex=ex)
+        nelz, nely, nelx = ex.segmentation.shape
+        lc = lattice_connectivity(ex=ex)
 
-    ec = element_connectivity(ex=ex, lattice=lc)
+        ec = element_connectivity(ex=ex, lattice=lc)
 
-    assert np.all(
-        ec == ex.gold_elements
-    ), "Calculated element connectivity error."
+        assert np.all(
+            ec == ex.gold_elements
+        ), "Calculated element connectivity error."
 
-    # TODO: Optimization: construct edges and draw each edge only
-    # once.
-    # edges = [element_edges(connectivity=x) for x in ec]
+        # TODO: Optimization: construct edges and draw each edge only
+        # once.
+        # edges = [element_edges(connectivity=x) for x in ec]
 
-    # save the numpy data as a .npy file
-    np.save(output_npy, ex.segmentation)
-    print(f"Saved: {output_npy}")
+        # save the numpy data as a .npy file
+        np.save(output_npy, ex.segmentation)
+        print(f"Saved: {output_npy}")
 
-    # to load the array back from the .npy file,
-    # use the numpy.load function:
-    loaded_array = np.load(output_npy)
+        # to load the array back from the .npy file,
+        # use the numpy.load function:
+        loaded_array = np.load(output_npy)
 
-    # verify the loaded array
-    print(loaded_array)
+        # verify the loaded array
+        print(loaded_array)
 
-    assert np.all(loaded_array == ex.segmentation)
+        assert np.all(loaded_array == ex.segmentation)
 
-    # now that the .npy file has been created and verified,
-    # move it to the repo at ~/autotwin/automesh/tests/input
+        # now that the .npy file has been created and verified,
+        # move it to the repo at ~/autotwin/automesh/tests/input
 
-    if not visualize:
-        return
+        if not visualize:
+            return
 
-    # visualization
+        # visualization
 
-    # Define the dimensions of the lattice
-    nxp, nyp, nzp = (nelx + 1, nely + 1, nelz + 1)
+        # Define the dimensions of the lattice
+        nxp, nyp, nzp = (nelx + 1, nely + 1, nelz + 1)
 
-    # Create a figure and a 3D axis
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection="3d")
+        # Create a figure and a 3D axis
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection="3d")
 
-    # Generate the lattice points
-    x = []
-    y = []
-    z = []
-    labels = []
+        # Generate the lattice points
+        x = []
+        y = []
+        z = []
+        labels = []
 
-    # Generate the element points
-    xel = []
-    yel = []
-    zel = []
-    # generate a set from the element connectivity
-    ec_set = set(ec.flatten())
+        # Generate the element points
+        xel = []
+        yel = []
+        zel = []
+        # generate a set from the element connectivity
+        ec_set = set(ec.flatten())
 
-    lattice_ijk = 0
-    for k in range(nzp):
-        for j in range(nyp):
-            for i in range(nxp):
-                x.append(i)
-                y.append(j)
-                z.append(k)
-                if lattice_ijk + 1 in ec_set:
-                    xel.append(i)
-                    yel.append(j)
-                    zel.append(k)
-                lattice_ijk += 1
-                labels.append(f"{lattice_ijk}: ({i},{j},{k})")
+        lattice_ijk = 0
+        for k in range(nzp):
+            for j in range(nyp):
+                for i in range(nxp):
+                    x.append(i)
+                    y.append(j)
+                    z.append(k)
+                    if lattice_ijk + 1 in ec_set:
+                        xel.append(i)
+                        yel.append(j)
+                        zel.append(k)
+                    lattice_ijk += 1
+                    labels.append(f" {lattice_ijk}: ({i},{j},{k})")
 
-    # Plot the lattice coordinates
-    ax.scatter(
-        x,
-        y,
-        z,
-        s=10,
-        facecolors="blue",
-        edgecolors="none",
-    )
+        # Plot the lattice coordinates
+        ax.scatter(
+            x,
+            y,
+            z,
+            s=10,
+            facecolors="blue",
+            edgecolors="none",
+        )
 
-    # Label the lattice coordinates
-    for idx, label in enumerate(labels):
-        ax.text(x[idx], y[idx], z[idx], label, color="red")
+        # Label the lattice coordinates
+        for idx, label in enumerate(labels):
+            # ax.text(x[idx], y[idx], z[idx], label, color="darkgray",
+            # fontsize=8)
+            ax.text(x[idx], y[idx], z[idx], label, color="darkgray")
 
-    # Plot the nodes included in the finite element connectivity
-    ax.scatter(
-        xel,
-        yel,
-        zel,
-        s=50,
-        facecolors="none",
-        edgecolors="blue",
-    )
+        # Plot the nodes included in the finite element connectivity
+        ax.scatter(
+            xel,
+            yel,
+            zel,
+            s=50,
+            facecolors="none",
+            edgecolors="blue",
+        )
 
-    # Set labels for the axes
-    ax.set_xlabel("x")
-    ax.set_ylabel("y")
-    ax.set_zlabel("z")
+        # Set labels for the axes
+        ax.set_xlabel("x")
+        ax.set_ylabel("y")
+        ax.set_zlabel("z")
 
-    x_ticks = list(range(nxp))
-    y_ticks = list(range(nyp))
-    z_ticks = list(range(nzp))
+        x_ticks = list(range(nxp))
+        y_ticks = list(range(nyp))
+        z_ticks = list(range(nzp))
 
-    ax.set_xticks(x_ticks)
-    ax.set_yticks(y_ticks)
-    ax.set_zticks(z_ticks)
+        ax.set_xticks(x_ticks)
+        ax.set_yticks(y_ticks)
+        ax.set_zticks(z_ticks)
 
-    ax.set_xlim(float(x_ticks[0]), float(x_ticks[-1]))
-    ax.set_ylim(float(y_ticks[0]), float(y_ticks[-1]))
-    ax.set_zlim(float(z_ticks[0]), float(z_ticks[-1]))
+        ax.set_xlim(float(x_ticks[0]), float(x_ticks[-1]))
+        ax.set_ylim(float(y_ticks[0]), float(y_ticks[-1]))
+        ax.set_zlim(float(z_ticks[0]), float(z_ticks[-1]))
 
-    # Set the camera view
-    ax.set_aspect("equal")
-    ax.view_init(elev=el, azim=az, roll=roll)
+        # Set the camera view
+        ax.set_aspect("equal")
+        ax.view_init(elev=el, azim=az, roll=roll)
 
-    # Set the title
-    ax.set_title(ex.figure_title)
+        # Set the title
+        ax.set_title(ex.figure_title)
 
-    # Add a footnote
-    # Get the current date and time in UTC
-    now_utc = datetime.datetime.now(datetime.UTC)
-    # Format the date and time as a string
-    timestamp_utc = now_utc.strftime("%Y-%m-%d %H:%M:%S UTC")
-    fn = f"Created with {__file__}\non {timestamp_utc}."
-    fig.text(0.5, 0.01, fn, ha="center", fontsize=8)
+        # Add a footnote
+        # Get the current date and time in UTC
+        now_utc = datetime.datetime.now(datetime.UTC)
+        # Format the date and time as a string
+        timestamp_utc = now_utc.strftime("%Y-%m-%d %H:%M:%S UTC")
+        fn = f"Figure: {output_png_short} "
+        fn += f"created with {__file__}\non {timestamp_utc}."
+        fig.text(0.5, 0.01, fn, ha="center", fontsize=8)
 
-    # Show the plot
-    plt.show()
+        # Show the plot
+        plt.show()
 
-    # plt.show()
-    fig.savefig(output_png, dpi=dpi)
-    print(f"Saved: {output_png}")
+        # plt.show()
+        fig.savefig(output_png, dpi=dpi)
+        print(f"Saved: {output_png}")
 
 
 if __name__ == "__main__":
--- tests/voxels.rs
+++ tests/voxels.rs
@@ -4,7 +4,7 @@
 //!
 
 use ndarray::Array3;
-use voxels_data::{Double, Single};
+use voxels_data::{Cube, Double, DoubleY, Quadruple, QuadrupleVoid, Single, Triple};
 
 mod voxels_data;
 
@@ -22,7 +22,7 @@ fn user() {
 fn single() {
     // item
     let ii = Single::new();
-    assert_eq!(ii.segmentation, Array3::from_elem((1, 1, 1), 1));
+    assert_eq!(ii.segmentation, [[[1]]]);
     assert_eq!(ii.scale, [1.0, 1.0, 1.0]);
     assert_eq!(ii.translate, [0.0, 0.0, 0.0]);
     // lattice
@@ -36,13 +36,138 @@ fn single() {
 #[test]
 fn double() {
     let ii = Double::new();
-    assert_eq!(ii.segmentation, Array3::from_elem((1, 1, 2), 1));
+    assert_eq!(ii.segmentation, [[[1, 1]]]);
     assert_eq!(ii.scale, [1.0, 1.0, 1.0]);
     assert_eq!(ii.translate, [0.0, 0.0, 0.0]);
     let ll = Double::gold_lattice();
     assert_eq!(ll, [[1, 2, 5, 4, 7, 8, 11, 10], [2, 3, 6, 5, 8, 9, 12, 11]]);
     let ee = Double::gold_elements();
     assert_eq!(ee, [[1, 2, 5, 4, 7, 8, 11, 10], [2, 3, 6, 5, 8, 9, 12, 11]]);
+}
+
+#[test]
+fn double_y() {
+    let ii = DoubleY::new();
+    assert_eq!(ii.segmentation, [[[1], [1]]]);
+    assert_eq!(ii.scale, [1.0, 1.0, 1.0]);
+    assert_eq!(ii.translate, [0.0, 0.0, 0.0]);
+    let ll = DoubleY::gold_lattice();
+    assert_eq!(ll, [[1, 2, 4, 3, 7, 8, 10, 9], [3, 4, 6, 5, 9, 10, 12, 11]]);
+    let ee = DoubleY::gold_elements();
+    assert_eq!(ee, [[1, 2, 4, 3, 7, 8, 10, 9], [3, 4, 6, 5, 9, 10, 12, 11]]);
+}
+
+#[test]
+fn triple() {
+    let ii = Triple::new();
+    assert_eq!(ii.segmentation, [[[1, 1, 1]]]);
+    assert_eq!(ii.scale, [1.0, 1.0, 1.0]);
+    assert_eq!(ii.translate, [0.0, 0.0, 0.0]);
+    let ll = Triple::gold_lattice();
+    assert_eq!(
+        ll,
+        [
+            [1, 2, 6, 5, 9, 10, 14, 13],
+            [2, 3, 7, 6, 10, 11, 15, 14],
+            [3, 4, 8, 7, 11, 12, 16, 15],
+        ]
+    );
+    let ee = Triple::gold_elements();
+    assert_eq!(
+        ee,
+        [
+            [1, 2, 6, 5, 9, 10, 14, 13],
+            [2, 3, 7, 6, 10, 11, 15, 14],
+            [3, 4, 8, 7, 11, 12, 16, 15],
+        ]
+    );
+}
+
+#[test]
+fn quadruple() {
+    let ii = Quadruple::new();
+    assert_eq!(ii.segmentation, [[[1, 1, 1, 1]]]);
+    assert_eq!(ii.scale, [1.0, 1.0, 1.0]);
+    assert_eq!(ii.translate, [0.0, 0.0, 0.0]);
+    let ll = Quadruple::gold_lattice();
+    assert_eq!(
+        ll,
+        [
+            [1, 2, 7, 6, 11, 12, 17, 16],
+            [2, 3, 8, 7, 12, 13, 18, 17],
+            [3, 4, 9, 8, 13, 14, 19, 18],
+            [4, 5, 10, 9, 14, 15, 20, 19],
+        ]
+    );
+    let ee = Quadruple::gold_elements();
+    assert_eq!(
+        ee,
+        [
+            [1, 2, 7, 6, 11, 12, 17, 16],
+            [2, 3, 8, 7, 12, 13, 18, 17],
+            [3, 4, 9, 8, 13, 14, 19, 18],
+            [4, 5, 10, 9, 14, 15, 20, 19],
+        ]
+    );
+}
+
+#[test]
+fn quadruple_void() {
+    let ii = QuadrupleVoid::new();
+    assert_eq!(ii.segmentation, [[[1, 0, 0, 1]]]);
+    assert_eq!(ii.scale, [1.0, 1.0, 1.0]);
+    assert_eq!(ii.translate, [0.0, 0.0, 0.0]);
+    let ll = QuadrupleVoid::gold_lattice();
+    assert_eq!(
+        ll,
+        [
+            [1, 2, 7, 6, 11, 12, 17, 16],
+            [2, 3, 8, 7, 12, 13, 18, 17],
+            [3, 4, 9, 8, 13, 14, 19, 18],
+            [4, 5, 10, 9, 14, 15, 20, 19],
+        ]
+    );
+    let ee = QuadrupleVoid::gold_elements();
+    assert_eq!(
+        ee,
+        [[1, 2, 7, 6, 11, 12, 17, 16], [4, 5, 10, 9, 14, 15, 20, 19],]
+    );
+}
+
+#[test]
+fn cube() {
+    let ii = Cube::new();
+    assert_eq!(ii.segmentation, Array3::from_elem((1, 2, 2), 1));
+    assert_eq!(ii.scale, [1.0, 1.0, 1.0]);
+    assert_eq!(ii.translate, [0.0, 0.0, 0.0]);
+    let ll = Cube::gold_lattice();
+    assert_eq!(
+        ll,
+        [
+            [1, 2, 5, 4, 10, 11, 14, 13],
+            [2, 3, 6, 5, 11, 12, 15, 14],
+            [4, 5, 8, 7, 13, 14, 17, 16],
+            [5, 6, 9, 8, 14, 15, 18, 17],
+            [10, 11, 14, 13, 19, 20, 23, 22],
+            [11, 12, 15, 14, 20, 21, 24, 23],
+            [13, 14, 17, 16, 22, 23, 26, 25],
+            [14, 15, 18, 17, 23, 24, 27, 26],
+        ]
+    );
+    let ee = Cube::gold_elements();
+    assert_eq!(
+        ee,
+        [
+            [1, 2, 5, 4, 10, 11, 14, 13],
+            [2, 3, 6, 5, 11, 12, 15, 14],
+            [4, 5, 8, 7, 13, 14, 17, 16],
+            [5, 6, 9, 8, 14, 15, 18, 17],
+            [10, 11, 14, 13, 19, 20, 23, 22],
+            [11, 12, 15, 14, 20, 21, 24, 23],
+            [13, 14, 17, 16, 22, 23, 26, 25],
+            [14, 15, 18, 17, 23, 24, 27, 26],
+        ]
+    );
 }
 
 /// Adds two numbers together.  Why doesn't this appear in the documentation?
