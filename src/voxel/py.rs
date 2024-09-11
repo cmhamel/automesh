ed89f29be7c28a0d328a78f8937e23a8f8a07ab8
mrbuche <mrbuche@sandia.gov>
2024-09-11 15:01:51 -0600

    snazzy

--- src/voxel/py.rs
+++ src/voxel/py.rs
@@ -11,6 +11,7 @@ pub fn register_module(parent_module: &Bound<'_, PyModule>) -> PyResult<()> {
     Ok(())
 }
 
+/// The voxels class.
 #[pyclass]
 pub struct Voxels {
     data: VoxelData,
@@ -18,26 +19,51 @@ pub struct Voxels {
 
 #[pymethods]
 impl Voxels {
+    #[doc = svgbobdoc::transform!(
+    /// Converts the voxels type into a finite element type.
+    ///
+    /// The voxel data can be scaled and translated (in that order).
+    ///
+    /// $$
+    /// x \mapsto s_x x + t_x\qquad y \mapsto s_y y + t_y\qquad z \mapsto s_z z + t_z
+    /// $$
+    ///
+    /// ```svgbob
+    ///                     8       7
+    ///                      *-------*
+    ///                   5 /|    6 /|
+    ///  z                 *-+-----* |
+    ///   ^  y             | |4    | |3
+    ///   | ^              | *-----|-*
+    ///   |/               |/      |/
+    ///   +-----> x        *-------*
+    ///                    1       2
+    /// ```
+    )]
     #[pyo3(signature = (scale=[1.0, 1.0, 1.0], translate=[0.0, 0.0, 0.0]))]
     pub fn as_finite_elements(&self, scale: Scale, translate: Translate) -> FiniteElements {
         let (element_blocks, element_connectivity, nodal_coordinates) =
             finite_element_data_from_npy_data(&self.data, &scale, &translate);
         FiniteElements::from_data(element_blocks, element_connectivity, nodal_coordinates)
     }
+    /// The internal voxels data.
     #[getter]
     pub fn get_data<'py>(&self, python: Python<'py>) -> Bound<'py, PyArray3<u8>> {
         self.data.to_pyarray_bound(python)
     }
+    /// Constructs and returns a new voxels type from an NPY file.
     #[staticmethod]
     pub fn from_npy(file_path: &str) -> Self {
         let data = voxel_data_from_npy(file_path);
         Self { data }
     }
+    /// Constructs and returns a new voxels type from an SPN file.
     #[staticmethod]
     pub fn from_spn(file_path: &str, nel: Nel) -> Self {
         let data = voxel_data_from_spn(file_path, nel);
         Self { data }
     }
+    /// Writes the internal voxels data to a NPY file.
     pub fn write_npy(&self, file_path: &str) {
         write_voxels_to_npy(&self.data, file_path);
     }
