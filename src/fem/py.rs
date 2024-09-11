c8b909835d339544a21db01cca8a83e1c5aa8dae
mrbuche <mrbuche@sandia.gov>
2024-09-11 15:01:51 -0600

    snazzy

--- src/fem/py.rs
+++ src/fem/py.rs
@@ -9,6 +9,7 @@ pub fn register_module(parent_module: &Bound<'_, PyModule>) -> PyResult<()> {
     Ok(())
 }
 
+/// The finite element type.
 #[pyclass]
 pub struct FiniteElements {
     element_blocks: ElementBlocks,
@@ -18,6 +19,7 @@ pub struct FiniteElements {
 
 #[pymethods]
 impl FiniteElements {
+    /// Constructs and returns a new Exodus type from data.
     #[new]
     pub fn from_data(
         element_blocks: ElementBlocks,
@@ -30,10 +32,12 @@ impl FiniteElements {
             nodal_coordinates,
         }
     }
+    /// The block for each finite element.
     #[getter]
     pub fn get_element_blocks<'py>(&self, python: Python<'py>) -> Bound<'py, PyArray1<usize>> {
         PyArray1::from_vec_bound(python, self.element_blocks.clone())
     }
+    /// The nodal connectivity for each finite element.
     #[getter]
     pub fn get_element_connectivity<'py>(
         &self,
@@ -41,13 +45,16 @@ impl FiniteElements {
     ) -> Bound<'py, PyArray2<usize>> {
         PyArray2::from_vec2_bound(python, &self.element_connectivity).unwrap()
     }
+    /// The nodal coordinates for each finite element.
     #[getter]
     pub fn get_nodal_coordinates<'py>(&self, python: Python<'py>) -> Bound<'py, PyArray2<f64>> {
         PyArray2::from_vec2_bound(python, &self.nodal_coordinates).unwrap()
     }
+    /// Writes the finite element data to a new Abaqus input file.
     pub fn write_inp(&self, file_path: &str) {
         Abaqus::write_inp(self, file_path)
     }
+    /// Writes the finite element data to a new Exodus mesh file.
     pub fn write_exo(&self, file_path: &str) {
         Exodus::write_exo(self, file_path)
     }
