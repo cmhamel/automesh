bfd890e6ad6b1b016e89db84f2c8be14f49fc1b8
mrbuche <mrbuche@sandia.gov>
2024-10-10 15:04:28 -0600

    tests

--- tests/voxel.py
+++ tests/voxel.py
@@ -4,6 +4,6 @@ from automesh import Voxels
 
 def test_write_npy():
     Voxels.from_spn('tests/input/letter_f_3d.spn', [4, 5, 3]) \
-        .write_npy('target/letter_f_3d.npy')
+          .write_npy('target/letter_f_3d.npy')
     assert (np.load('tests/input/letter_f_3d.npy') ==
             np.load('target/letter_f_3d.npy')).all()
