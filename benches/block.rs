00e4bd967d15d9ea207c72f4fbd346ec679781e1
mrbuche <mrbuche@sandia.gov>
2024-10-15 11:15:45 -0600

    trying out benchmarks

--- benches/block.rs
+++ benches/block.rs
@@ -1,64 +1,106 @@
 #![feature(test)]
 
 extern crate test;
+use automesh::{Abaqus, Voxels};
 use test::Bencher;
 
-use automesh::Voxels;
-
-const NEL: [usize; 3] = [10, 10, 10];
 const REMOVE: Option<Vec<u8>> = None;
 const SCALE: [f64; 3] = [1.0, 1.0, 1.0];
 const TRANSLATE: [f64; 3] = [0.0, 0.0, 0.0];
 
-// some seem to scale better than others
-// so need both small and large benches to show what
-
-#[bench]
-fn from_npy(bencher: &mut Bencher) {
-    bencher.iter(|| Voxels::from_npy("benches/block.npy"));
+macro_rules! bench_block {
+    ($nel:expr) => {
+        const NEL: [usize; 3] = [$nel, $nel, $nel];
+        #[bench]
+        fn calculate_nodal_hierarchy(bencher: &mut Bencher) -> Result<(), String> {
+            let voxels = Voxels::from_spn(&format!("benches/block_{}.spn", $nel), NEL)?;
+            let mut fem = voxels.into_finite_elements(REMOVE, &SCALE, &TRANSLATE)?;
+            fem.calculate_node_element_connectivity()?;
+            fem.calculate_node_node_connectivity()?;
+            bencher.iter(|| fem.calculate_nodal_hierarchy().unwrap());
+            Ok(())
+        }
+        #[bench]
+        fn calculate_node_element_connectivity(bencher: &mut Bencher) -> Result<(), String> {
+            let voxels = Voxels::from_spn(&format!("benches/block_{}.spn", $nel), NEL)?;
+            let mut fem = voxels.into_finite_elements(REMOVE, &SCALE, &TRANSLATE)?;
+            bencher.iter(|| fem.calculate_node_element_connectivity().unwrap());
+            Ok(())
+        }
+        #[bench]
+        fn calculate_node_node_connectivity(bencher: &mut Bencher) -> Result<(), String> {
+            let voxels = Voxels::from_spn(&format!("benches/block_{}.spn", $nel), NEL)?;
+            let mut fem = voxels.into_finite_elements(REMOVE, &SCALE, &TRANSLATE)?;
+            fem.calculate_node_element_connectivity()?;
+            bencher.iter(|| fem.calculate_node_node_connectivity().unwrap());
+            Ok(())
+        }
+        #[bench]
+        fn from_npy(bencher: &mut Bencher) {
+            let npy = format!("benches/block_{}.npy", $nel);
+            bencher.iter(|| Voxels::from_npy(&npy).unwrap());
+        }
+        #[bench]
+        fn from_npy_into_finite_elements(bencher: &mut Bencher) {
+            let npy = format!("benches/block_{}.npy", $nel);
+            bencher.iter(|| {
+                Voxels::from_npy(&npy)
+                    .unwrap()
+                    .into_finite_elements(REMOVE, &SCALE, &TRANSLATE)
+                    .unwrap()
+            });
+        }
+        #[bench]
+        fn from_spn(bencher: &mut Bencher) {
+            let spn = format!("benches/block_{}.spn", $nel);
+            bencher.iter(|| Voxels::from_spn(&spn, NEL).unwrap());
+        }
+        #[bench]
+        fn from_spn_into_finite_elements(bencher: &mut Bencher) {
+            let spn = format!("benches/block_{}.spn", $nel);
+            bencher.iter(|| {
+                Voxels::from_spn(&spn, NEL)
+                    .unwrap()
+                    .into_finite_elements(REMOVE, &SCALE, &TRANSLATE)
+                    .unwrap()
+            });
+        }
+        #[bench]
+        fn write_inp(bencher: &mut Bencher) -> Result<(), String> {
+            let voxels = Voxels::from_spn(&format!("benches/block_{}.spn", $nel), NEL)?;
+            let fem = voxels.into_finite_elements(REMOVE, &SCALE, &TRANSLATE)?;
+            let inp = format!("target/block_{}.inp", $nel);
+            bencher.iter(|| fem.write_inp(&inp).unwrap());
+            Ok(())
+        }
+        #[bench]
+        fn write_npy(bencher: &mut Bencher) -> Result<(), String> {
+            let voxels = Voxels::from_spn(&format!("benches/block_{}.spn", $nel), NEL)?;
+            let npy = format!("target/block_{}.npy", $nel);
+            bencher.iter(|| voxels.write_spn(&npy).unwrap());
+            Ok(())
+        }
+        #[bench]
+        fn write_spn(bencher: &mut Bencher) -> Result<(), String> {
+            let voxels = Voxels::from_spn(&format!("benches/block_{}.spn", $nel), NEL)?;
+            let spn = format!("target/block_{}.spn", $nel);
+            bencher.iter(|| voxels.write_spn(&spn).unwrap());
+            Ok(())
+        }
+    };
 }
 
-#[bench]
-fn from_npy_into_finite_elements(bencher: &mut Bencher) {
-    bencher.iter(|| {
-        Voxels::from_npy("benches/block.npy")
-            .unwrap()
-            .into_finite_elements(REMOVE, &TRANSLATE, &SCALE)
-    });
+mod block_8 {
+    use super::*;
+    bench_block!(8);
 }
 
-#[bench]
-fn from_spn(bencher: &mut Bencher) {
-    bencher.iter(|| Voxels::from_spn("benches/block.spn", NEL));
+mod block_16 {
+    use super::*;
+    bench_block!(16);
 }
 
-#[bench]
-fn from_spn_into_finite_elements(bencher: &mut Bencher) {
-    bencher.iter(|| {
-        Voxels::from_spn("benches/block.spn", NEL)
-            .unwrap()
-            .into_finite_elements(REMOVE, &TRANSLATE, &SCALE)
-    });
+mod block_32 {
+    use super::*;
+    bench_block!(32);
 }
-
-#[bench]
-fn write_npy(bencher: &mut Bencher) -> Result<(), String> {
-    let voxels = Voxels::from_spn("benches/block.spn", NEL)?;
-    bencher.iter(|| voxels.write_spn("target/block.npy"));
-    Ok(())
-}
-
-#[bench]
-fn write_spn(bencher: &mut Bencher) -> Result<(), String> {
-    let voxels = Voxels::from_spn("benches/block.spn", NEL)?;
-    bencher.iter(|| voxels.write_spn("target/block.spn"));
-    Ok(())
-}
-
-// #[bench]
-// fn from_npy(bencher: &mut Bencher) {
-//     let voxels = Voxels::from_npy("block.npy");
-//     bencher.iter(||
-//         voxels.
-//     );
-// }
