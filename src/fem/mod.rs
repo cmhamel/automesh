35d399e29de3a23aa9172234eeab98020a6400a5
mrbuche <mrbuche@sandia.gov>
2024-10-24 13:39:35 -0600

    working in tests

--- src/fem/mod.rs
+++ src/fem/mod.rs
@@ -9,9 +9,10 @@ use std::time::Instant;
 
 use super::{ELEMENT_NUMBERING_OFFSET, NODE_NUMBERING_OFFSET, NSD};
 use chrono::Utc;
+use netcdf::{create, Error as ErrorNetCDF};
 use std::{
     fs::File,
-    io::{BufWriter, Error, Write},
+    io::{BufWriter, Error as ErrorIO, Write},
 };
 
 const ELEMENT_TYPE: &str = "C3D8R";
@@ -434,7 +435,7 @@ impl FiniteElements {
         }
     }
     /// Writes the finite elements data to a new Abaqus input file.
-    pub fn write_inp(&self, file_path: &str) -> Result<(), Error> {
+    pub fn write_inp(&self, file_path: &str) -> Result<(), ErrorIO> {
         write_fem_to_inp(
             file_path,
             self.get_element_blocks(),
@@ -443,22 +444,109 @@ impl FiniteElements {
         )
     }
 
-    pub fn write_exo(&self, file_path: &str) -> Result<(), netcdf::Error> {
+    pub fn write_exo(&self, file_path: &str) -> Result<(), ErrorNetCDF> {
         write_fem_to_exo(
             file_path,
             self.get_element_blocks(),
             self.get_element_node_connectivity(),
-            self.get_nodal_coordinates()
+            self.get_nodal_coordinates(),
         )
     }
+}
+
+fn write_fem_to_exo(
+    file_path: &str,
+    element_blocks: &Blocks,
+    element_node_connectivity: &Connectivity,
+    nodal_coordinates: &Coordinates,
+) -> Result<(), ErrorNetCDF> {
+    let mut file = create(file_path)?;
+    file.add_attribute::<f32>("api_version", 8.25)?;
+    file.add_attribute::<i32>("file_size", 1)?;
+    file.add_attribute::<i32>("floating_point_word_size", 8)?;
+    file.add_attribute::<f32>("version", 8.25)?;
+    let mut element_blocks_unique = element_blocks.clone();
+    element_blocks_unique.sort();
+    element_blocks_unique.dedup();
+    file.add_dimension("num_dim", NSD)?;
+    file.add_dimension("num_elem", element_blocks.len())?;
+    file.add_dimension("num_el_blk", element_blocks_unique.len())?;
+    let mut eb_prop1 = file.add_variable::<i32>("eb_prop1", &["num_el_blk"])?;
+    element_blocks_unique
+        .iter()
+        .enumerate()
+        .try_for_each(|(index, unique_block)| eb_prop1.put_value(*unique_block as i32, index))?;
+    let mut block_renumbered = 0;
+    let mut num_el_in_blk = "num_el_in_blk0".to_string();
+    let mut num_nod_per_el = "num_nod_per_el0".to_string();
+    #[cfg(feature = "profile")]
+    let time = Instant::now();
+    element_blocks_unique
+        .into_iter()
+        .try_for_each(|unique_block| {
+            block_renumbered += 1;
+            num_el_in_blk = format!("num_el_in_blk{}", block_renumbered);
+            num_nod_per_el = format!("num_nod_per_el{}", block_renumbered);
+            file.add_dimension(
+                &num_el_in_blk,
+                element_blocks
+                    .iter()
+                    .filter(|&block| block == &unique_block)
+                    .count(),
+            )?;
+            file.add_dimension(&num_nod_per_el, ELEMENT_NUM_NODES)?;
+            let mut connectivities = file.add_variable::<i32>(
+                format!("connect{}", block_renumbered).as_str(),
+                &[&num_el_in_blk, &num_nod_per_el],
+            )?;
+            element_blocks
+                .iter()
+                .enumerate()
+                .filter(|(_, &block)| block == unique_block)
+                .enumerate()
+                .try_for_each(|(index, (element, _))| {
+                    connectivities.put_attribute("elem_type", "HEX8")?;
+                    connectivities.put_values(
+                        &element_node_connectivity[element]
+                            .iter()
+                            .map(|entry| *entry as i32)
+                            .collect::<Vec<i32>>(),
+                        (index, ..),
+                    )
+                })?;
+            Ok::<_, ErrorNetCDF>(())
+        })?;
+    #[cfg(feature = "profile")]
+    println!(
+        "             \x1b[1;93mElement-to-node connectivity\x1b[0m {:?}",
+        time.elapsed()
+    );
+    #[cfg(feature = "profile")]
+    let time = Instant::now();
+    let xs: Vec<f64> = nodal_coordinates.iter().map(|coords| coords[0]).collect();
+    let ys: Vec<f64> = nodal_coordinates.iter().map(|coords| coords[1]).collect();
+    let zs: Vec<f64> = nodal_coordinates.iter().map(|coords| coords[2]).collect();
+    file.add_dimension("num_nodes", nodal_coordinates.len())?;
+    file.add_variable::<f64>("coordx", &["num_nodes"])?
+        .put_values(&xs, 0..)?;
+    file.add_variable::<f64>("coordy", &["num_nodes"])?
+        .put_values(&ys, 0..)?;
+    file.add_variable::<f64>("coordz", &["num_nodes"])?
+        .put_values(&zs, 0..)?;
+    #[cfg(feature = "profile")]
+    println!(
+        "           \x1b[1;93m⤷ Nodal coordinates\x1b[0m {:?}",
+        time.elapsed()
+    );
+    Ok(())
 }
 
 fn write_fem_to_inp(
     file_path: &str,
     element_blocks: &Blocks,
     element_node_connectivity: &Connectivity,
-    nodal_coordinates: &Coordinates
-) -> Result<(), Error> {
+    nodal_coordinates: &Coordinates,
+) -> Result<(), ErrorIO> {
     let element_number_width = get_width(element_node_connectivity);
     let node_number_width = get_width(nodal_coordinates);
     let inp_file = File::create(file_path)?;
@@ -475,7 +563,7 @@ fn write_fem_to_inp(
     file.flush()
 }
 
-fn write_heading_to_inp(file: &mut BufWriter<File>) -> Result<(), Error> {
+fn write_heading_to_inp(file: &mut BufWriter<File>) -> Result<(), ErrorIO> {
     let heading = format!(
         "*HEADING\nautotwin.automesh\nversion {}\nautogenerated on {}",
         env!("CARGO_PKG_VERSION"),
@@ -489,7 +577,7 @@ fn write_nodal_coordinates_to_inp(
     file: &mut BufWriter<File>,
     nodal_coordinates: &Coordinates,
     node_number_width: &usize,
-) -> Result<(), Error> {
+) -> Result<(), ErrorIO> {
     #[cfg(feature = "profile")]
     let time = Instant::now();
     file.write_all("*NODE, NSET=ALLNODES".as_bytes())?;
@@ -526,13 +614,13 @@ fn write_element_node_connectivity_to_inp(
     element_node_connectivity: &Connectivity,
     element_number_width: &usize,
     node_number_width: &usize,
-) -> Result<(), Error> {
+) -> Result<(), ErrorIO> {
     #[cfg(feature = "profile")]
     let time = Instant::now();
-    let mut unique_element_blocks = element_blocks.clone();
-    unique_element_blocks.sort();
-    unique_element_blocks.dedup();
-    unique_element_blocks
+    let mut element_blocks_unique = element_blocks.clone();
+    element_blocks_unique.sort();
+    element_blocks_unique.dedup();
+    element_blocks_unique
         .iter()
         .clone()
         .try_for_each(|current_block| {
@@ -565,7 +653,7 @@ fn write_element_node_connectivity_to_inp(
                 })?;
             end_section(file)
         })?;
-    let result = unique_element_blocks.iter().try_for_each(|block| {
+    let result = element_blocks_unique.iter().try_for_each(|block| {
         file.write_all(
             format!(
                 "*SOLID SECTION, ELSET=EB{}, MATERIAL=Default-Steel\n",
@@ -582,92 +670,18 @@ fn write_element_node_connectivity_to_inp(
     result
 }
 
-fn end_section(file: &mut BufWriter<File>) -> Result<(), Error> {
+fn end_section(file: &mut BufWriter<File>) -> Result<(), ErrorIO> {
     file.write_all(&[10, 42, 42, 10])
 }
 
-fn delimiter(file: &mut BufWriter<File>) -> Result<(), Error> {
+fn delimiter(file: &mut BufWriter<File>) -> Result<(), ErrorIO> {
     file.write_all(&[44, 32])
 }
 
-fn indent(file: &mut BufWriter<File>) -> Result<(), Error> {
+fn indent(file: &mut BufWriter<File>) -> Result<(), ErrorIO> {
     file.write_all(&[10, 32, 32, 32, 32])
 }
 
 fn get_width<T>(input: &[T]) -> usize {
     input.len().to_string().chars().count()
-}
-
-// exodus output methods below
-// Maybe should think about making a generic
-// output file type with appropriate traits
-fn write_fem_to_exo (
-    file_path: &str,
-    element_blocks: &Blocks,
-    element_node_connectivity: &Connectivity,
-    nodal_coordinates: &Coordinates
-) -> Result<(), netcdf::Error> {
-    let element_number_width = get_width(element_node_connectivity);
-    let node_number_width = get_width(nodal_coordinates);
-    let mut file = netcdf::create(file_path)?;
-
-    // global initialization information
-    file.add_dimension("num_dim", 3)?;
-    file.add_dimension("num_nodes", node_number_width)?;
-    file.add_dimension("num_elem", element_number_width)?;
-    file.add_dimension("num_el_blk", element_blocks.len())?;
-    file.add_dimension("num_node_sets", 0)?;
-    file.add_dimension("num_side_sets", 0)?;
-
-    // nodal variables
-    // need to do one by one or the compiler yells at you
-    // some weird borrows going on in netcdf
-    let mut coords_x = file.add_variable::<f64>("coordx", &["num_nodes"])?;
-    let xs: Vec<f64> = nodal_coordinates
-        .iter()
-        .map(|x| x[0])
-        .collect();
-    coords_x.put_values(&xs, (0, ..))?;
-    let mut coords_y = file.add_variable::<f64>("coordy", &["num_nodes"])?;
-    let ys: Vec<f64> = nodal_coordinates
-        .iter()
-        .map(|x| x[0])
-        .collect();
-    coords_y.put_values(&ys, (0, ..))?;
-    let mut coords_z = file.add_variable::<f64>("coordz", &["num_nodes"])?;
-    let zs: Vec<f64> = nodal_coordinates
-        .iter()
-        .map(|x| x[0])
-        .collect();
-    coords_z.put_values(&zs, (0, ..))?;
-
-    // block specific information
-    // element_blocks
-    //     .iter()
-    //     .enumerate()
-    //     .try_for_each(|(block_id, block)| {
-    //         let var_name = format!("num_nod_per_el{block_id}");
-    //         file.add_dimension(&var_name, 8);
-    //         let var_name = format!("num_el_per_blk{block_id}");
-    //         file.add_dimension(&var_name, *block);
-    //         Ok::<(), netcdf::Error>(())
-    //     });
-    // I'm a little confused on what blocks is storing based on the tests
-    // so I'm just writing to a single block for now.
-    file.add_dimension("num_node_per_el1", 8)?; // 8 for 8 noded hex
-    file.add_dimension("num_el_per_blk1", element_node_connectivity.len())?; 
-    
-    // this is giving an issue below
-    // then we're stuck choosing an integer type
-    // let mut conn = file.add_variable::<usize>("connect1", &["num_elem"])?;
-    // element_node_connectivity
-    //     .iter()
-    //     .enumerate()
-    //     .try_for_each(|(element, connectivity)| {
-    //         let n = 8 * element;
-    //         conn.put_values(&connectivity, (n, ..));
-    //         Ok::<(), netcdf::Error>(())
-    //     });
-    
-    Ok(())
 }
