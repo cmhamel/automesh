2272fca3e80b234ef68bafb87957283a59b8d304
mrbuche <mrbuche@sandia.gov>
2024-11-11 08:55:33 -0700

    more tests

--- tests/voxel.py
+++ tests/voxel.py
@@ -7,3 +7,15 @@ def test_write_npy():
           .write_npy('target/letter_f_3d.npy')
     assert (np.load('tests/input/letter_f_3d.npy') ==
             np.load('target/letter_f_3d.npy')).all()
+
+
+def test_write_spn():
+    input = 'tests/input/letter_f_3d.spn'
+    output = 'target/letter_f_3d.spn'
+    Voxels.from_spn(input, [4, 5, 3]) \
+          .write_spn(output)
+    with open(input) as gold, open(output) as file:
+        line = file.readline()
+        while line != '':
+            assert gold.readline() == line
+            line = file.readline()
