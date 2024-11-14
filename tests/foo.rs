c2b0d5f4895265322f8a2b16562cd821d0cd030f
mrbuche <mrbuche@sandia.gov>
2024-11-14 16:00:30 -0700

    something like this

--- tests/foo.rs
+++ tests/foo.rs
@@ -5,6 +5,10 @@ type Points = Vec<Point>;
 
 #[derive(Debug)]
 struct Cell {
+    fac_u: Vec<usize>,
+    fac_d: Vec<usize>,
+    fac_l: Vec<usize>,
+    fac_r: Vec<usize>,
     level: usize,
     min_x: f64,
     min_y: f64,
@@ -42,6 +46,11 @@ impl Cell {
     }
 }
 
+const X_MIN: f64 = 0.0;
+const X_MAX: f64 = 8.0;
+const Y_MIN: f64 = 0.0;
+const Y_MAX: f64 = 8.0;
+
 #[test]
 fn foo() {
     let m = 6;
@@ -50,13 +59,52 @@ fn foo() {
         Point::new([5.2, 2.3]),
         Point::new([6.6, 6.6]),
     ];
-    let mut tree = vec![Cell {
-        level: 0,
-        min_x: 0.0,
-        min_y: 0.0,
-        max_x: 8.0,
-        max_y: 8.0,
-    }];
+    let mut tree = vec![
+        Cell {
+            fac_u: vec![2],
+            fac_d: vec![],
+            fac_l: vec![],
+            fac_r: vec![1],
+            level: 1,
+            min_x: X_MIN,
+            min_y: Y_MIN,
+            max_x: 0.5 * (X_MIN + X_MAX),
+            max_y: 0.5 * (Y_MIN + Y_MAX),
+        },
+        Cell {
+            fac_u: vec![3],
+            fac_d: vec![],
+            fac_l: vec![0],
+            fac_r: vec![],
+            level: 1,
+            min_x: 0.5 * (X_MIN + X_MAX),
+            min_y: Y_MIN,
+            max_x: X_MAX,
+            max_y: 0.5 * (Y_MIN + Y_MAX),
+        },
+        Cell {
+            fac_u: vec![],
+            fac_d: vec![0],
+            fac_l: vec![],
+            fac_r: vec![3],
+            level: 1,
+            min_x: X_MIN,
+            min_y: 0.5 * (Y_MIN + Y_MAX),
+            max_x: 0.5 * (X_MIN + X_MAX),
+            max_y: Y_MAX,
+        },
+        Cell {
+            fac_u: vec![],
+            fac_d: vec![1],
+            fac_l: vec![2],
+            fac_r: vec![],
+            level: 1,
+            min_x: 0.5 * (X_MIN + X_MAX),
+            min_y: 0.5 * (Y_MIN + Y_MAX),
+            max_x: X_MAX,
+            max_y: Y_MAX,
+        },
+    ];
     let mut cell;
     let mut index = 0;
     let mut level;
@@ -76,8 +124,18 @@ fn foo() {
             max_y = cell.get_max_y();
             val_x = 0.5 * (min_x + max_x);
             val_y = 0.5 * (min_y + max_y);
+            //
+            // (1) if you remove the parent cell at the end, you are changing the cell numbering for cells after!
+            // (2) new cell numbers are easy since they fit at end of vec
+            // (3) for each neighbor of cell, need to replace cell in the neighbor list by some subset of below
+            // (4) fill out neighbors of new cells conversely
+            //
             tree.extend([
                 Cell {
+                    fac_u: vec![],
+                    fac_d: vec![],
+                    fac_l: vec![],
+                    fac_r: vec![],
                     level,
                     min_x: *min_x,
                     min_y: *min_y,
@@ -85,6 +143,10 @@ fn foo() {
                     max_y: val_y,
                 },
                 Cell {
+                    fac_u: vec![],
+                    fac_d: vec![],
+                    fac_l: vec![],
+                    fac_r: vec![],
                     level,
                     min_x: val_x,
                     min_y: *min_y,
@@ -92,6 +154,10 @@ fn foo() {
                     max_y: val_y,
                 },
                 Cell {
+                    fac_u: vec![],
+                    fac_d: vec![],
+                    fac_l: vec![],
+                    fac_r: vec![],
                     level,
                     min_x: *min_x,
                     min_y: val_y,
@@ -99,6 +165,10 @@ fn foo() {
                     max_y: *max_y,
                 },
                 Cell {
+                    fac_u: vec![],
+                    fac_d: vec![],
+                    fac_l: vec![],
+                    fac_r: vec![],
                     level,
                     min_x: val_x,
                     min_y: val_y,
@@ -106,7 +176,21 @@ fn foo() {
                     max_y: *max_y,
                 },
             ]);
-            tree.remove(index); // would keeping the "parent" cells help with balancing somehow? you could store "garbage" indices and remove them from the tree later (take out the garbage)
+            tree.remove(index);
+            // would keeping the "parent" cells help with balancing somehow?
+            // you could store "garbage" indices and remove them from the tree later (take out the garbage)
+            // or maybe you can move it into another Vec and loop over those during the balancing part?
+            // or use the coordinates of a cell and contains() for all other cells? that would be slow, though.
+            // or construct some sort of neighboring list on-the-fly to check against?
+            // wont you need some sort of neighbor list for the primal-to-dual transition too?
+            //
+            // do you want to template the Tree to change NSD based on NSD in the input points? might help with testing
+            //
+            // during balancing, you can at least rule out further subdividing the smallest 2 sizes:
+            // loop through level 1's => subdivide if any neighbors level 3 or below
+            // loop through level 2's => subdivide if any neighbors level 4 or below
+            // repeat until hit level (m - 1)
+            //
         } else {
             index += 1;
         }
