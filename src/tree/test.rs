683484b78527c76c587bd5465929b5aa7e7f97dd
mrbuche <mrbuche@sandia.gov>
2024-11-18 16:26:59 -0700

    yep need to iterate balance until nothing changes

--- src/tree/test.rs
+++ src/tree/test.rs
@@ -3,9 +3,12 @@ use flavio::math::Tensor;
 
 #[test]
 fn foobar() -> Result<(), ErrorIO> {
-    let levels = 3;
+    let levels = 5;
     let points = vec![Point::new([0.499, 0.499, 0.499])];
     let mut tree = OcTree::from_points(&levels, &points, [0.0, 1.0, 0.0, 1.0, 0.0, 1.0]);
+    tree.balance(&levels);
+    tree.balance(&levels);
+    tree.balance(&levels);
     tree.balance(&levels);
     tree.write_mesh("primal.mesh")
 }
