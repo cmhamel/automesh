ea31898a7334dfce52e2a38663606cc1268d2893
Chad Hovey <hovey@me.com>
2025-01-29 16:37:14 -0500

    one facet and two facet read and write tests

--- src/tessellation/mod.rs
+++ src/tessellation/mod.rs
@@ -14,29 +14,24 @@ use conspire::math::{Tensor, TensorArray};
 use std::fmt;
 use std::fs::File;
 use std::io::{BufWriter, Error};
-use std::ops::Index;
+// use std::ops::Index;
 // use std::io::{self, Write};
 // use std::path::Path;
 use stl_io::{read_stl, IndexedMesh, IndexedTriangle, Normal, Vertex};
 //use stl_io::{read_stl, write_stl};
 
 /// The tessellation type.
+#[derive(Debug, PartialEq)]
 pub struct Tessellation {
     data: IndexedMesh,
 }
 
 /// Inherent implementation of the tessellation type
 impl Tessellation {
-    // /// Create a new tessellation from a file.
-    // pub fn new<P: AsRef<std::path::Path>>(path: P) -> Self {
-    //     // let data = read_stl(&mut path).unwrap();
-    //     // let data = read_stl(&mut path).expect("Failed to read STL file.");
-    //     // Open the file and create a file handle
-    //     let mut file = File::open(path).expect("Failed to open STL file.");
-    //     let data = read_stl(&mut file).expect("Failed to read STL file.");
-    //     Self { data }
-    // }
-
+    /// Construct a tessellation from an IndexedMesh.
+    pub fn new(indexed_mesh: IndexedMesh) -> Self {
+        Self { data: indexed_mesh }
+    }
     /// Constructs a tessellation from finite elements, consuming the finite elements.
     pub fn from_finite_elements(finite_elements: TriangularFiniteElements) -> Self {
         let mut normal = Vector::zero();
@@ -86,17 +81,10 @@ impl Tessellation {
     pub fn write_stl(&self, file_path: &str) -> Result<(), Error> {
         write_tessellation_to_stl(self.get_data(), file_path)
     }
-    // pub fn write_stl<P: AsRef<std::path::Path>>(&self, path: P) -> io::Result<()> {
-    //     let mut file = File::create(path)?; // Create a new file for writing
-    //     // #TODO: Ask MRB
-    //     write_stl(&mut file, &self.data)?; // Write the data to the file
-    //     // write_stl(&mut file, self.data.clone())?; // Write the data to the file
-    //     Ok(())
-    // }
 }
 
-fn write_tessellation_to_stl(data: &IndexedMesh, file_path: &str) -> Result<(), Error> {
-    let mut file = BufWriter::new(File::create(file_path)?);
+fn write_tessellation_to_stl(_data: &IndexedMesh, file_path: &str) -> Result<(), Error> {
+    let mut _file = BufWriter::new(File::create(file_path)?);
     // stl_io::write_stl(&mut file, &data.iter()) // #TODO: morph IndexedMesh into a list that can iter()
     Ok(())
 }
@@ -112,200 +100,3 @@ impl fmt::Display for Tessellation {
         )
     }
 }
-
-// use super::{Vector, NSD};
-// use conspire::math::TensorArray;
-
-// /// The multiplying scale in each direction.
-// pub struct Scale(Vector);
-//
-// impl Scale {
-//     pub fn x(&self) -> &f64 {
-//         &self.0[0]
-//     }
-//     pub fn y(&self) -> &f64 {
-//         &self.0[1]
-//     }
-//     pub fn z(&self) -> &f64 {
-//         &self.0[2]
-//     }
-// }
-//
-// impl From<[f64; NSD]> for Scale {
-//     fn from(scale: [f64; NSD]) -> Self {
-//         if scale.iter().any(|&entry| entry <= 0.0) {
-//             panic!("Need to specify scale > 0.")
-//         } else {
-//             Self(Vector::new(scale))
-//         }
-//     }
-// }
-
-// #[derive(Debug)]
-// struct Vertex {
-//     x: f64,
-//     y: f64,
-//     z: f64,
-// }
-//
-// impl fmt::Display for Vertex {
-//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-//         write!(f, "vertex {:.6e} {:.6e} {:.6e}\n", self.x, self.y, self.z)
-//     }
-// }
-//
-// #[derive(Debug)]
-// struct Normal {
-//     x: f64,
-//     y: f64,
-//     z: f64,
-// }
-//
-// impl fmt::Display for Normal {
-//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-//         write!(f, "normal {:.6e} {:.6e} {:.6e}\n", self.x, self.y, self.z)
-//     }
-// }
-//
-// #[derive(Debug)]
-// struct OuterLoop {
-//     vertices: Vec<Vertex>,
-// }
-//
-// impl fmt::Display for OuterLoop {
-//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-//         let vertices_str: Vec<String> = self.vertices.iter().map(|v| v.to_string()).collect();
-//         write!(f, "\nouter loop\n{}\nendloop", vertices_str.join(""))
-//     }
-// }
-//
-// #[derive(Debug)]
-// struct Facet {
-//     normal: Normal,
-//     outer_loop: OuterLoop,
-// }
-//
-// impl fmt::Display for Facet {
-//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-//         write!(f, "facet normal {}\n    {}", self.normal, self.outer_loop)
-//     }
-// }
-//
-//
-// #[derive(Debug)]
-// struct Tessellation {
-//     name: String,
-//     facets: Vec<Facet>,
-// }
-
-// fn read_stl<P: AsRef<Path>>(path: P) -> io::Result<Vec<Facet>> {
-//     let file = File::open(path)?;
-//     let reader = io::BufReader::new(file);
-//     let mut facets = Vec::new();
-//     let mut current_facet: Option<Facet> = None;
-//
-//     for line in reader.lines() {
-//         let line = line?;
-//         let trimmed = line.trim();
-//
-//         if trimmed.starts_with("facet normal") {
-//             // Parse the normal vector
-//             // example:
-//             // facet normal 0.000000e+00 -1.000000e+00 0.000000e+00
-//             let parts: Vec<&str> = trimmed.split_whitespace().collect();
-//             let normal = Vertex {
-//                 x: parts[2].parse().unwrap(), // 2 -> x-index
-//                 y: parts[3].parse().unwrap(), // 3 -> y-index
-//                 z: parts[4].parse().unwrap(), // 4 -> z-index
-//             };
-//             current_facet = Some(Facet {
-//                 normal,
-//                 outer_loop: OuterLoop {
-//                     vertices: Vec::new(),
-//                 },
-//             });
-//         } else if trimmed.starts_with("vertex") {
-//             if let Some(ref mut facet) = current_facet {
-//                 // Parse the vertex
-//                 // example
-//                 // vertex 0.000000e+00 0.000000e+00 1.000000e+00
-//                 let parts: Vec<&str> = trimmed.split_whitespace().collect();
-//                 let vertex = Vertex {
-//                     x: parts[1].parse().unwrap(), // 1 -> x-index
-//                     y: parts[2].parse().unwrap(), // 2 -> y-index
-//                     z: parts[3].parse().unwrap(), // 3 -> z-index
-//                 };
-//                 facet.outer_loop.vertices.push(vertex);
-//             }
-//         } else if trimmed == "endfacet" {
-//             if let Some(facet) = current_facet.take() {
-//                 facets.push(facet);
-//             }
-//         }
-//     }
-//
-//     Ok(facets)
-// }
-
-// fn main() -> io::Result<()> {
-//     println!("Hello, world!");
-//
-//     let vertex = Vertex {
-//         x: 1.0,
-//         y: 0.0,
-//         z: 1.0,
-//     };
-//
-//     println!("{}", vertex);
-//
-//
-//     let facets = read_stl("/Users/chovey/autotwin/automesh/tests/input/single.stl")?;
-//
-//     for facet in facets {
-//         // println!("{:?}", facet);
-//         println!("{}", facet);
-//     }
-//
-//     let aa: Vec<f64> = vec![1.0, 2.0, 3.0];
-//     println!("aa {:?}", aa);
-//
-//     let mut aa2 = Vec::new();
-//     aa2.push(2.0);
-//     aa2.push(3.0);
-//     aa2.push(4.0);
-//     println!("aa2 {:?}", aa2);
-//
-//     // Create a new vector of length NSD, initialized with 0.0
-//     let vec_nsd: Vec<f64> = vec![0.0; NSD];
-//
-//     // Print the vector
-//     println!("vec_nsd {:?}", vec_nsd);
-//
-//     // Given scale as usize
-//     let scale: usize = 2;
-//
-//     // Convert usize to f64
-//     let scale_f64: f64 = scale as f64;
-//
-//     let bb: Vec<f64> = aa.iter().map(|&x| x * scale_f64).collect();
-//     println!("bb {:?}", bb);
-//
-//     let cc: Vec<f64> = aa.iter().map(|&x| x * x).collect();
-//     println!("cc {:?}", cc);
-//
-//     let dd: Vec<f64> = aa.iter().map(|&x| square(x)).collect();
-//     println!("dd {:?}", dd);
-//
-//     let ee: Vec<f64> = dd.iter().map(|&x| square_root(x)).collect();
-//     println!("ee {:?}", ee);
-//
-//     Ok(())
-// }
-//
-// fn square(x: f64) -> f64 {
-//     x * x
-// }
-//
-// fn square_root(x: f64) -> f64 {
-//     x.sqrt()
-// }
